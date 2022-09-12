fedba20c0c1d95172b105847baca350a6b09e5ab
amaneph <amanuelephrem44@gmail.com>
2022-09-12 13:04:09 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - starts here
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i=0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
