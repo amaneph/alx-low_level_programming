d3e7c0bb41132353b33426bf99c39bcbe88e3224
amaneph <amanuelephrem44@gmail.com>
2022-09-12 12:49:39 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print if the nuber is positive, zero, or negative
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	vor (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
