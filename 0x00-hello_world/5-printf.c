dba7a3a0386cf52cadf83defcbd4d9245df0905f
amaneph <amanuelephrem44@gmail.com>
2022-09-12 04:33:40 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - where the code starts running
+ *
+ * return - returns 0 after successful completion
+ */
+
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+	
