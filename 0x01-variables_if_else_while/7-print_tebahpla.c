82a99660ded3d6479e85ef1b9decb9b85a425a78
amaneph <amanuelephrem44@gmail.com>
2022-09-12 13:36:47 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - everything starts here
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
