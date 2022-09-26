5e29a7a2c0194b6f11b6f32bee996b3049f22ddb
amaneph <amanuelephrem44@gmail.com>
2022-09-26 08:05:15 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,14 +4,14 @@
  * @dest: buffer where we will copy to
  * @src: what we are to copy
  * @n: n bytes of @src
- *       
+ *
  * Return: Always 0 (Success)
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-		
+
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 
